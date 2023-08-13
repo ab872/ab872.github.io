ce37a8aa6afc07cc70df1395f23cd7c31ebc78f5
A. Bright <ab8720843@gmail.com>
2023-08-13 09:30:50 +0800

    index.js     adjust layout Chrome Android

--- index.js
+++ index.js
@@ -1355,7 +1355,7 @@ if (deviceScreenWidth == 1143) {
 
 	//  id="grid-containerMisc"
      	//  grid-template-rows: 0.3vh 190vh 0.3vh
-	document.getElementById('grid-containerMisc').style.gridTemplateRows = "0.3vh 330vh 0.3vh";
+	document.getElementById('grid-containerMisc').style.gridTemplateRows = "0.3vh 390vh 0.3vh";
 
 
 
@@ -1363,6 +1363,12 @@ if (deviceScreenWidth == 1143) {
      	//  grid-template-rows: 0.3vh 95vh 0.3vh
 	document.getElementById('grid-containerWindows').style.gridTemplateRows = "0.3vh 65vh 0.3vh";
 
+
+	//  adjust the size of the
+	//  Bootstrap icon
+	//  id="BootstrapIcon"           
+	document.getElementById("BootstrapIcon").style.width="96px"; 
+	document.getElementById("BootstrapIcon").style.height="96px";
 
 
    //   end     screen width - others
